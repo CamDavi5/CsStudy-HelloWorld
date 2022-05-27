0f9433c8083359b3ebd06220736915082129de4c
CamDavi5 <camrdavis.biz@gmail.com>
2022-05-27 16:13:02 -0500

    Add project files.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("\nWhat is your name?");
+var name = Console.ReadLine();
+
+Console.WriteLine($"\nHello {name}!");
